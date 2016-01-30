8456ba6b17ede41431e935e6069e521fc6f7d3c2
thehambone <thehambone93@gmail.com>
2016-01-30 15:50:43 -0600

    Now returns correct path for Mac OS X

--- src/thehambone/gtatools/gta3savefileeditor/game/Game.java
+++ src/thehambone/gtatools/gta3savefileeditor/game/Game.java
@@ -2,12 +2,14 @@ package thehambone.gtatools.gta3savefileeditor.game;
 
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+import thehambone.gtatools.gta3savefileeditor.Main;
 
 /**
  * Contains methods relating to the Grand Theft Auto III game.
  * 
  * This class may be removed in the future because I'm not sure what else would
- * go in here other than getGameUserDirectoryPath().
+ go in here other than getUserDirectoryPath().
  * 
  * @author thehambone
  * @version 0.1
@@ -15,10 +17,24 @@ import javax.swing.JFileChooser;
  */
 public class Game
 {
-    public static String getGameUserDirectoryPath()
+    /**
+     * Returns the path to the "GTA3 User Files" folder.
+     * 
+     * @return the path to the "GTa3 User Files" folder
+     */
+    public static String getUserDirectoryPath()
     {
-        // todo: should probably do this through native code - reading the registry and whatnot
-        return new JFileChooser().getFileSystemView().getDefaultDirectory().getPath() + File.separator + "GTA3 User Files";
-        // getFileSystemView().getDefaultDirectory() essentailly gets the user's "My Documents" folder
+        FileSystemView fsv = new JFileChooser().getFileSystemView();
+        String documentsPath = fsv.getDefaultDirectory().getPath();
+        
+        String gta3UserFiles = documentsPath + File.separator;
+        if (Main.getOperatingSystem() == Main.OperatingSystem.MAC_OS_X) {
+            gta3UserFiles += "Rockstar Games" + File.separator
+                    + "GTA3 User Files";
+        } else {
+            gta3UserFiles += "GTA3 User Files";
+        }
+        
+        return gta3UserFiles;
     }
 }
