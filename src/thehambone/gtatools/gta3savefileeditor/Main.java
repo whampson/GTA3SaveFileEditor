ea8d1761b7a3c8c3f0d511739b12470feff3f137
thehambone <thehambone93@gmail.com>
2016-02-07 01:09:35 -0600

    Cleaned up code

--- src/thehambone/gtatools/gta3savefileeditor/Main.java
+++ src/thehambone/gtatools/gta3savefileeditor/Main.java
@@ -1,6 +1,7 @@
 package thehambone.gtatools.gta3savefileeditor;
 
 import java.awt.Image;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
@@ -15,6 +16,7 @@ import thehambone.gtatools.gta3savefileeditor.gxt.GXT;
 import thehambone.gtatools.gta3savefileeditor.util.GUIUtilities;
 import thehambone.gtatools.gta3savefileeditor.util.Logger;
 import thehambone.gtatools.gta3savefileeditor.util.ResourceLoader;
+import thehambone.gtatools.gta3savefileeditor.util.UncaughtExceptionHandler;
 
 
 /**
@@ -32,9 +34,11 @@ public final class Main
     public static final String PROGRAM_AUTHOR = "thehambone";
     public static final String PROGRAM_AUTHOR_EMAIL = "thehambone93@gmail.com";
     public static final String PROGRAM_AUTHOR_URL
-            = "http://gtaforums.com/user/907241-thehambone/";
+            = "http://gtaforums.com/index.php?showuser=907241";
     public static final String PROGRAM_UPDATE_URL
             = "http://www.gtagarage.com/mods/show.php?id=27101";
+    public static final String PROJECT_PAGE_URL
+            = "http://gtaforums.com/index.php?showtopic=784598";
     public static final String PROGRAM_COPYRIGHT
             = "Copyright (C) 2015-2016 thehambone";
     
@@ -45,15 +49,16 @@ public final class Main
     private static final String GXT_PATH
             = "META-INF/res/mobile/american.gxt";
     
-    private static final Logger.Level DEFAULT_LOGGING_LEVEL
-            = Logger.Level.DEBUG;   // TODO: CHANGE THIS FOR RELEASE
+    private static final Logger.Level DEFAULT_LOGGING_LEVEL = Logger.Level.INFO;
     
     private static Date buildDate = new Date(0);
     private static int buildNumber = 0; 
-    private static OperatingSystem os = OperatingSystem.UNKNOWN;
-    private static boolean isDebugModeEnabled = false;
     
-        
+    private static OperatingSystem os = OperatingSystem.UNKNOWN;
+    
+    private static boolean isDebugModeEnabled = false;
+    private static boolean showHelpMessage = false;
+    
     /**
      * Checks whether debug mode is enabled.
      * 
@@ -95,38 +100,36 @@ public final class Main
     {
         return buildNumber;
     }
-     
+    
+    /**
+     * The program entry point.
+     * 
+     * @param args command-line arguments
+     */
     public static void main(String[] args)
     {
-        /* TODO:
-         * -BUG: "Exception while removing reference" at shutdown; shutdown hook
-         *       related? (IDE only?)
-         * -cmd options: --log-to-file
-         * -Make file backups
-         * -update timestamp on save
-         * -Tweak about page text
-         * -About dialog copyright text put copyright holder
-         * -Better crashdump output
-         * -Recent files: remove duplicates/push duplicate to top
-         * -Recent files: update list on load
-         * -Documentation
-         * -Tooltip text
-         * -Reorder imports
-         * -Add newline to end of all files
+        /* TODO (0.3):
+         * -Generate crash dumps when a fatal error occurs, include info about
+         *  the state of the program and a save file varaible dump
+         * -Document savefile package
          */
         
         initLogger();
-        initUncaughtExceptionHandler();
-        initLookAndFeel();
         parseCommandLineArgs(args);
+        initLookAndFeel();
+        initUncaughtExceptionHandler();
         determineOS();
         readBuildProperties();
+        
+        if (showHelpMessage) {
+            showHelpMessage();
+        }
         
         Logger.info("%s\n", PROGRAM_TITLE);
         Logger.info("%s <%s>\n", PROGRAM_COPYRIGHT, PROGRAM_AUTHOR_EMAIL);
         Logger.info("Version %s build %d\n", PROGRAM_VERSION, buildNumber);
-        Logger.info("Compiled on %s\n", new SimpleDateFormat("MMM. dd, yyyy")
-                .format(buildDate));
+        Logger.info("Compiled on %s\n",
+                new SimpleDateFormat("MMM. dd, yyyy").format(buildDate));
         
         Logger.info("os.name = %s\n", System.getProperty("os.name"));
         Logger.info("os.version = %s\n", System.getProperty("os.version"));
@@ -142,7 +145,7 @@ public final class Main
         createAndShowGUI(icon);
     }
     
-    /*
+    /**
      * Sets up the status logger.
      */
     private static void initLogger()
@@ -150,17 +153,64 @@ public final class Main
         Logger.newInstance(DEFAULT_LOGGING_LEVEL);
     }
     
-    /*
-     * Sets the default uncaught exception handler for all threads.
+    /**
+     * Analyzes command-line arguments.
      */
-    private static void initUncaughtExceptionHandler()
+    private static void parseCommandLineArgs(String[] args)
     {
-        Logger.debug("Setting default uncaught exception handler...");
-        Thread.setDefaultUncaughtExceptionHandler(
-                new UncaughtExceptionHandler());
+        if (args.length == 0) {
+            return;
+        }
+        
+        Logger.debug("Parsing command-line options...");
+        for (String arg : args) {
+            if (!arg.startsWith("--")) {
+                continue;
+            }
+            
+            Logger.debug("Parsing argument: %s\n", arg);
+            
+            String[] tokens = arg.split("=");   // format: --argument=option
+            switch (tokens[0].substring(2)) {
+                case "debug":
+                    isDebugModeEnabled = true;
+                    Logger.info("Debug mode enabled!");
+                    break;
+                case "help":
+                    showHelpMessage = true;
+                    break;
+                case "log-level":
+                    if (tokens.length < 2) {
+                        Logger.warn("Option not specified for 'log-level' "
+                                + "- argument will be ignored");
+                    } else {
+                        try {
+                            Logger.Level l = Logger.Level.valueOf(tokens[1]);
+                            Logger.getLogger().setLevel(l);
+                            Logger.info("Logging level set to %s\n", l.name());
+                        } catch (IllegalArgumentException ex) {
+                            Logger.warn("Invalid logging level. Using default "
+                                    + "logging level (%s) [%s: %s]\n",
+                                    DEFAULT_LOGGING_LEVEL,
+                                    ex.getClass().getName(), ex.getMessage());
+                        }
+                    }
+                    break;
+                case "log-to-file":
+                    try {
+                        Logger.getLogger().logToFile(true);
+                        Logger.info("Logging to file enabled!");
+                    } catch (FileNotFoundException ex) {
+                        Logger.error("Unable to create log file.", ex);
+                    }
+                    break;
+                default:
+                    Logger.warn("Unknown command-line option: " + arg);
+            }
+        }
     }
     
-    /*
+    /**
      * Sets the application look and feel to match the system's look and feel.
      */
     private static void initLookAndFeel()
@@ -178,9 +228,19 @@ public final class Main
         }
     }
     
-    /*
-     * Determines on which operating system the program is running using the
-     * OS name.
+    /**
+     * Sets the default uncaught exception handler for all threads.
+     */
+    private static void initUncaughtExceptionHandler()
+    {
+        Logger.debug("Setting default uncaught exception handler...");
+        Thread.setDefaultUncaughtExceptionHandler(
+                new UncaughtExceptionHandler());
+    }
+    
+    /**
+     * Determines on which operating system the program is running by analyzing
+     * the OS name.
      */
     private static void determineOS()
     {
@@ -202,7 +262,7 @@ public final class Main
         Logger.debug("Operating system: " + os.name());
     }
     
-    /*
+    /**
      * Reads build information from a file embedded in the JAR.
      */
     private static void readBuildProperties()
@@ -247,8 +307,21 @@ public final class Main
         Logger.debug("Build properties loaded successfully!");
     }
     
-    /*
-     * Initializes tasks to be perfomed when the program closes. 
+    /**
+     * Displays a message box containing valid command-line arguments.
+     */
+    private static void showHelpMessage()
+    {
+        String helpString = "Command-line options:\n\n"
+                + "--debug\n"
+                + "--help\n"
+                + "--log-level=&#60;DEBUG | INFO | WARN | ERROR | FATAL&#62;\n"
+                + "--log-to-file";
+        GUIUtilities.showInformationMessageBox(null, helpString, "Help", 350);
+    }
+    
+    /**
+     * Defines actions to be performed when the JVM closes.
      */
     private static void initShutdownHooks()
     {
@@ -273,63 +346,62 @@ public final class Main
         Logger.debug("\"Save-Settings\" hook initialized!");
     }
     
-    /*
-     * Analyzes command-line arguments.
+    /**
+     * Loads program settings from a file.
      */
-    private static void parseCommandLineArgs(String[] args)
+    private static void loadSettings()
     {
-        if (args.length == 0) {
+        Logger.info("Loading program settings...");
+        
+        try {
+            Settings.load();
+        } catch (IOException ex) {
+            String errMsg = "Failed to load program settings. "
+                    + "Default settings will be used.";
+            Logger.warn(errMsg, ex);
+            Logger.stackTrace(ex);
+            GUIUtilities.showErrorMessageBox(null, errMsg, "IO Error", ex);
+            Settings.loadDefaults();
             return;
         }
         
-        Logger.debug("Parsing command-line options...");
-        for (String arg : args) {
-            if (!arg.startsWith("--")) {
-                continue;
-            }
-            
-            Logger.debug("Parsing argument: %s\n", arg);
-            
-            String[] tokens = arg.split("=");   // format: --argument=option
-            switch (tokens[0].substring(2)) {
-                case "debug":
-                    isDebugModeEnabled = true;
-                    Logger.info("Debug mode enabled!");
-                    break;
-                case "help":
-                    String helpString = "Command-line options:\n\n"
-                            + "--debug\n"
-                            + "--help\n"
-                            + "--log-level=<DEBUG | INFO | WARN | ERROR "
-                            + "| FATAL>";
-                    GUIUtilities.showInformationMessageBox(
-                            null, helpString, "Help");
-                    break;
-                case "log-level":
-                    if (tokens.length < 2) {
-                        Logger.warn("Option not specified for 'log-level' "
-                                + "- argument will be ignored");
-                    } else {
-                        try {
-                            Logger.Level l = Logger.Level.valueOf(tokens[1]);
-                            Logger.getLogger().setLevel(l);
-                            Logger.info("Logging level set to %s\n", l.name());
-                        } catch (IllegalArgumentException ex) {
-                            Logger.warn("Invalid logging level. Using default "
-                                    + "logging level (%s) [%s: %s]\n",
-                                    DEFAULT_LOGGING_LEVEL,
-                                    ex.getClass().getName(), ex.getMessage());
-                        }
-                    }
-                    break;
-                default:
-                    Logger.warn("Unknown command-line option: " + arg);
-            }
+        String debugLogging = Settings.get(Settings.Key.DEBUG_LOGGING);
+        if (!Boolean.parseBoolean(debugLogging)) {
+            return;
+        }
+        
+        try {
+            Logger.getLogger().logToFile(true);
+            Logger.getLogger().setLevel(Logger.Level.DEBUG);
+            Logger.info("Debug logging enabled!");
+        } catch (FileNotFoundException ex) {
+            Logger.error("Unable to enable debug logging!", ex);
+            Logger.stackTrace(ex);
+            GUIUtilities.showErrorMessageBox(null,
+                    "Unable to enable debug logging!", "Error", ex);
         }
     }
     
-    /*
-     * Loads and returns the program icon.
+    /**
+     * Loads a GXT table from a GXT file embedded in the JAR.
+     */
+    private static void loadGXT()
+    {
+        Logger.info("Loading GXT table...");
+        try {
+            InputStream gxtStream = ResourceLoader.getResourceStream(GXT_PATH);
+            GXT.loadGXTTable(gxtStream);
+        } catch (IOException ex) {
+            String errMsg = "Failed to load GXT table. "
+                    + "Some features will be unavailable.";
+            Logger.error(errMsg, ex);
+            Logger.stackTrace(ex.fillInStackTrace());
+            GUIUtilities.showErrorMessageBox(null, errMsg, "IO Error", ex);
+        }
+    }
+      
+    /**
+     * Loads and returns the program icon from an image embedded in the JAR.
      */
     private static Image loadIcon()
     {
@@ -346,44 +418,8 @@ public final class Main
         return icon;
     }
     
-    /*
-     * Loads program settings from a file.
-     */
-    private static void loadSettings()
-    {
-        try {
-            Logger.info("Loading program settings...");
-            Settings.load();
-        } catch (IOException ex) {
-            String errMsg = "Failed to load program settings. "
-                    + "Default settings will be used.";
-            Logger.warn(errMsg, ex);
-            Logger.stackTrace(ex);
-            GUIUtilities.showErrorMessageBox(null, errMsg, "IO Error", ex);
-            Settings.loadDefaults();
-        }
-    }
-    
-    /*
-     * Loads a GXT table from a GXT file.
-     */
-    private static void loadGXT()
-    {
-        Logger.info("Loading GXT table...");
-        try {
-            InputStream gxtStream = ResourceLoader.getResourceStream(GXT_PATH);
-            GXT.loadGXTTable(gxtStream);
-        } catch (IOException ex) {
-            String errMsg = "Failed to load GXT table. "
-                    + "Some features will be unavailable.";
-            Logger.error(errMsg, ex);
-            Logger.stackTrace(ex.fillInStackTrace());
-            GUIUtilities.showErrorMessageBox(null, errMsg, "IO Error", ex);
-        }
-    }
-    
-    /*
-     * Loads the main GUI frame. 
+    /**
+     * Initializes and displays the main GUI frame.
      */
     private static void createAndShowGUI(final Image icon)
     {
